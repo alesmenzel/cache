448f7a91e377836a0b1acce538e71f5c0f0f37b4
Aleš Menzel <alesmenzel@gmail.com>
2018-11-11 12:12:07 +0100

    consistent argumentsId for objects with different key order

--- create-cache.js
+++ create-cache.js
@@ -1,21 +1,74 @@
 // @ts-nocheck
 const debug = require('debug')('cache:create-cache');
-
 const crypto = require('crypto');
 
-const noop = () => {};
 const cwd = process.cwd();
+
+/**
+ * Non-operational function
+ */
+const noop = () => {};
+
+/**
+ * Create MD5 hash from a string
+ *
+ * @param {String} str String to hash
+ */
 const md5 = str =>
   crypto
     .createHash('md5')
     .update(str)
     .digest('hex');
+
+/**
+ * Get filepath from stack beginning from cwd
+ *
+ * @param {String} str Stack
+ * @returns Filepath in the format of `/path/to/file.js:154:17`
+ */
 const filepath = str =>
   str
     .split('\n')[2]
     .match(/\((.*)\)/)[1] // Take "filepath:row:column"
     .replace(cwd, '') // Cross-system
     .replace(/\\/g, '/'); // Cross-platform
+
+/**
+ * Replace objects as array touples because objects key order is not guaranteed
+ * which could potentially cause different unique arguments keys
+ *
+ * @param {String} key Key
+ * @param {*} value Value
+ */
+const replaceObjects = (key, value) => {
+  if (value && !Array.isArray(value) && typeof value === 'object') {
+    return Object.keys(value)
+      .map(key => [key, value[key]])
+      .sort((a, b) => {
+        const keyA = a[0].toUpperCase();
+        const keyB = b[1].toUpperCase();
+
+        if (keyA < keyB) {
+          return -1;
+        }
+
+        if (keyA > keyB) {
+          return 1;
+        }
+
+        return 0;
+      });
+  }
+
+  return value;
+};
+
+/**
+ * Stringify arguments into a unique key
+ *
+ * @param {Array} args Arguments
+ */
+const stringify = args => md5(JSON.stringify(args, replaceObjects));
 
 /**
  * Create cache service
@@ -32,7 +85,7 @@ const createCache = (options = {}) => {
       ttl = 60 * 60, // 60 minutes
       precache = -1, // Disabled by default
       hash = md5,
-      resolve = args => md5(JSON.stringify(args)),
+      resolve = stringify,
     } = { ...options, ...overrides };
 
     if (!storage) {
